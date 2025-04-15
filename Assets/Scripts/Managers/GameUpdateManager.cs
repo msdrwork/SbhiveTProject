eed13024b069f8332ef6f6239631c29e318506f6
msdrWork <michaelangelosdrwork@gmail.com>
2025-04-16 01:11:44 +0800

    Add Game Update Manager
    Created a game update manager that would collect every other script's Update method

---
+++ Assets/Scripts/Managers/GameUpdateManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameUpdateManager : MonoBehaviour
+{
+    private bool isUpdating;
+    public bool IsUpdating
+    {
+        get
+        {
+            return isUpdating;
+        }
+    }
+
+    private List<IUpdateable> dirtyUpdatables;
+    private List<IUpdateable> currentUpdateables;
+    private bool isDirty;
+
+    public void Initialize()
+    {
+        dirtyUpdatables = new List<IUpdateable>();
+        currentUpdateables = new List<IUpdateable>();
+    }
+
+    public void SetUpdateable(IUpdateable updateable)
+    {
+        if (dirtyUpdatables == null)
+        {
+            dirtyUpdatables = new List<IUpdateable>();
+        }
+
+        dirtyUpdatables.Add(updateable);
+        isDirty = true;
+    }
+
+    public void Pause()
+    {
+        isUpdating = false;
+    }
+
+    public void Play()
+    {
+        isUpdating = true;
+    }
+
+    private void Update()
+    {
+        if (isUpdating)
+        {
+            for (int i = 0; i < currentUpdateables.Count; i++)
+            {
+                currentUpdateables[i].OnUpdate();
+            }
+        }
+
+        if (isDirty)
+        {
+            for (int i = 0; i < dirtyUpdatables.Count; i++)
+            {
+                currentUpdateables.Add(dirtyUpdatables[i]);
+            }
+
+            dirtyUpdatables.Clear();
+            isDirty = false;
+        }
+    }
+}
