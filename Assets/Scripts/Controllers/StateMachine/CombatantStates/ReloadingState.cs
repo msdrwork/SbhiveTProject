96019ef094b63f6083cf06dae3813e8286bfdce8
msdrWork <michaelangelosdrwork@gmail.com>
2025-04-16 01:23:00 +0800

    Add Reloading State

---
+++ Assets/Scripts/Controllers/StateMachine/CombatantStates/ReloadingState.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class ReloadingState : State<CombatantState>
+{
+    private Combatant combatant;
+
+    private float elapsedTime;
+
+    public ReloadingState(Combatant combatant, StateMachine<CombatantState> stateMachine) : base(stateMachine)
+    {
+        this.combatant = combatant;
+    }
+
+    public override void OnEnter()
+    {
+        elapsedTime = 0;
+    }
+
+    public override void OnUpdate()
+    {
+        if (combatant.CurrentState == CombatantState.Death)
+        {
+            combatant.SetState(CombatantState.Death);
+            stateMachine.ChangeState(CombatantState.Death);
+            return;
+        }
+
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime >= 1f / combatant.Weapon.AttackSpeed)
+        {
+            combatant.Weapon.Reload();
+            combatant.SetState(CombatantState.PrepareAttack);
+            stateMachine.ChangeState(CombatantState.PrepareAttack);
+        }
+    }
+}
