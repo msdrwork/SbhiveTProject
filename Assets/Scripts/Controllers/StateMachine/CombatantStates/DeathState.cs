359c6042dd8069f495888d827d624d63519b0e3e
msdrWork <michaelangelosdrwork@gmail.com>
2025-04-16 01:26:34 +0800

    Add Shooting and Death states

---
+++ Assets/Scripts/Controllers/StateMachine/CombatantStates/DeathState.cs
@@ -0,0 +1,21 @@
+﻿public class DeathState : State<CombatantState>
+{
+    private Combatant combatant;
+
+    public DeathState(Combatant combatant, StateMachine<CombatantState> stateMachine) : base(stateMachine)
+    {
+        this.combatant = combatant;
+    }
+
+    public override void OnEnter()
+    {
+        // Send Death Event
+        combatant.SetTarget(-1);
+        stateMachine.SetActive(false);
+    }
+
+    public override void OnUpdate()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Controllers/StateMachine/CombatantStates/ShootingState.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingState : State<CombatantState>
+{
+    private Dictionary<int, Combatant> allCombatants;
+    private Combatant combatant;
+    private Combatant targetCombatant;
+
+    public ShootingState(Combatant combatant, Dictionary<int, Combatant> allCombatants, StateMachine<CombatantState> stateMachine) : base(stateMachine)
+    {
+        this.allCombatants = allCombatants;
+        this.combatant = combatant;
+    }
+
+    public override void OnEnter()
+    {
+        if (combatant.CurrentState == CombatantState.Death)
+        {
+            combatant.SetState(CombatantState.Death);
+            stateMachine.ChangeState(CombatantState.Death);
+            return;
+        }
+
+        targetCombatant = allCombatants[combatant.CurrentTargetId];
+        combatant.FaceTarget(targetCombatant.transform.position);
+        combatant.Shoot(targetCombatant);
+        combatant.SetState(CombatantState.PrepareAttack);
+        stateMachine.ChangeState(CombatantState.PrepareAttack);
+
+    }
+
+    public override void OnUpdate()
+    {
+    }
+}
