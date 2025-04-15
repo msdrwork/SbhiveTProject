f1f6097804e6b5bca60f31f70185d78306e478cc
msdrWork <michaelangelosdrwork@gmail.com>
2025-04-16 00:15:00 +0800

    Add basic statemachine
    This PR adds a basic statemachine that is using an enum for the state Id's

---
+++ Assets/Scripts/Controllers/StateMachine/State.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public abstract class State<TState> where TState : Enum
+{
+    public StateMachine<TState> stateMachine;
+
+    public State(StateMachine<TState> stateMachine)
+    {
+        this.stateMachine = stateMachine;
+    }
+
+    public abstract void OnEnter();
+
+    public abstract void OnUpdate();
+}
---
+++ Assets/Scripts/Controllers/StateMachine/StateMachine.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class StateMachine<TState> : IUpdateable where TState : Enum
+{
+    private Dictionary<TState, State<TState>> States;
+    private State<TState> currentState;
+
+    private bool isActive;
+    private bool isDirty;
+
+    public StateMachine()
+    {
+        States = new Dictionary<TState, State<TState>>();
+        isActive = false;
+    }
+
+    public void AddState(TState stateId, State<TState> newState)
+    {
+        if (!States.ContainsKey(stateId))
+        {
+            States.Add(stateId, newState);
+        }
+        else
+        {
+            Debug.LogWarning("[StateMachine]: Cant add same state twice");
+        }
+    }
+
+    public void RemoveState(TState stateId)
+    {
+        if (States.ContainsKey(stateId))
+        {
+            States.Remove(stateId);
+        }
+    }
+
+    public void ChangeState(TState stateId)
+    {
+        currentState = States[stateId];
+        currentState.OnEnter();
+    }
+
+    public void OnUpdate()
+    {
+        if (isActive && currentState != null)
+        {
+            currentState.OnUpdate();
+        }
+    }
+
+    public void SetActive(bool isActive)
+    {
+        this.isActive = isActive;
+    }
+
+    public void Destroy()
+    {
+        States.Clear();
+    }
+}
