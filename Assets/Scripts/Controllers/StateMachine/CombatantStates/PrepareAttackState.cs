aa58da01f541769add68b4174e0eadb1fd711f3c
msdrWork <michaelangelosdrwork@gmail.com>
2025-04-16 01:22:49 +0800

    Add Pre Attack State

---
+++ Assets/Scripts/Controllers/StateMachine/CombatantStates/PrepareAttackState.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+public class PrepareAttackState : State<CombatantState>
+{
+    private Combatant combatant;
+    private Dictionary<int, Combatant> allCombatants;
+    private Combatant targetCombatant;
+
+    public PrepareAttackState(Combatant combatant, Dictionary<int, Combatant> allCombatants, StateMachine<CombatantState> stateMachine) : base(stateMachine)
+    {
+        this.combatant = combatant;
+        this.allCombatants = allCombatants;
+    }
+
+    public override void OnEnter()
+    {
+        if (combatant.CurrentState == CombatantState.Death)
+        {
+            combatant.SetState(CombatantState.Death);
+            stateMachine.ChangeState(CombatantState.Death);
+            return;
+        }
+
+        targetCombatant = allCombatants[combatant.CurrentTargetId];
+
+        if (targetCombatant.CurrentState == CombatantState.Death)
+        {
+            combatant.SetState(CombatantState.Idle);
+            stateMachine.ChangeState(CombatantState.Idle);
+        }
+        else if (combatant.Weapon.IsReloading)
+        {
+            combatant.SetState(CombatantState.Reloading);
+            stateMachine.ChangeState(CombatantState.Reloading);
+        }
+        else
+        {
+            combatant.SetState(CombatantState.Shooting);
+            stateMachine.ChangeState(CombatantState.Shooting);
+        }
+    }
+
+    public override void OnUpdate()
+    {
+
+    }
+}
